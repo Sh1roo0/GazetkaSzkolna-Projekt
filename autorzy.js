880afac614402157e5a4c08f43c451bd5d23a540
Sh1roo0 <kamil.przygrodzki4@gmail.com>
2025-11-12 19:59:44 +0100

    final-version

--- autorzy.js
+++ autorzy.js
@@ -88,3 +88,8 @@ function renderAuthors() {
 
 
 document.addEventListener('DOMContentLoaded', renderAuthors);
+
+
+
+      
+        
--- nav.js
+++ nav.js
@@ -1,11 +1,19 @@
-let menuList = document.getElementById("menuList")
-menuList.style.maxHeight = "0px";
 
-function toggleMenu() {
-    if (menuList.style.maxHeight == "0px") {
-        menuList.style.maxHeight = "300px";
-    }
-    else {
+
+        let burger = document.getElementById("burger");
+        let menuList = document.getElementById("menuList")
         menuList.style.maxHeight = "0px";
-    }
-}
+
+       burger.addEventListener("click", function toggleMenu(){
+         if(menuList.style.maxHeight == "0px")
+            {
+                menuList.style.maxHeight = "300px";
+            }
+            else{
+                menuList.style.maxHeight = "0px";
+            }
+       })   
+           
+        
+        
+    
